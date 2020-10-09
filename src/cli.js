f13460cf22c157b0933afc103fc8f079db83980a
Vladimir Osokin <osokin.home@gmail.com>
2020-10-09 17:12:53 +0300

    add cli argument for step

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,9 @@ const { readFile, writeFile } = require('fs/promises');
 const convertSvg = require('../index');
 
 module.exports = async function cli(args) {
-  const { file, output, bbox, pretty } = args;
+  const { file, output, bbox, pretty, step } = args;
   const svg = await readFile(file, 'utf8');
-  const geojson = await convertSvg(svg, bbox);
+  const geojson = await convertSvg(svg, bbox, step);
   const result = JSON.stringify(...(pretty ? [geojson, null, 2] : [geojson]));
   writeFile(output, result);
 };
