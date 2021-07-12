2c846644cb9cc96e43896c8ac19dc323e032609d
Vladimir Osokin <osokin.home@gmail.com>
2021-07-12 21:50:37 +0500

    setup typescript and configs

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const DOMParser = require('xmldom').DOMParser;
-const cleanSvg = require('./src/svgo');
-const getProjection = require('./src/projection');
-const getBounds = require('./src/bounds');
-const parseSvg = require('./src/parse');
-const turf = require('@turf/helpers');
-const rewind = require('@turf/rewind').default;
-const truncator = require('@turf/truncate').default;
+const DOMParser = require("xmldom").DOMParser;
+const turf = require("@turf/helpers");
+const rewind = require("@turf/rewind").default;
+const truncator = require("@turf/truncate").default;
+const cleanSvg = require("./src/svgo");
+const getProjection = require("./src/projection");
+const getBounds = require("./src/bounds");
+const parseSvg = require("./src/parse");
 
 /**
  *
  * @param {String} input
  * @param {{bbox?: number[], step?: number, truncate?: boolean | number}} options
- * @returns {turf.FeatureCollection}
+ * @returns {GeoJSON.FeatureCollection}
  */
 function convertSvg(input, options = {}) {
   const {
     bbox = [-180, -85.051129, 180, 85.051129],
     step = 1,
-    truncate = false
+    truncate = false,
   } = options;
   function reProject(x) {
     if (Array.isArray(x)) {
@@ -26,7 +26,7 @@ function convertSvg(input, options = {}) {
       return projection(x);
     }
   }
-  process.env.CURVE_SEGMENTATION_STEP = step + '';
+  process.env.CURVE_SEGMENTATION_STEP = step + "";
   bbox[1] = Math.max(bbox[1], -85.051129);
   bbox[3] = Math.min(bbox[3], 85.051129);
   const svg = new DOMParser().parseFromString(input);
@@ -35,7 +35,7 @@ function convertSvg(input, options = {}) {
   const features = parseSvg(svg);
   const geojson = features.map(({ type, geometry, id }) => {
     const chain = [{ f: reProject }, { f: turf[type], args: { id } }];
-    if (type === 'polygon') chain.push({ f: rewind });
+    if (type === "polygon") chain.push({ f: rewind });
     if (truncate !== false)
       chain.push({ f: truncator, args: { precision: truncate } });
     return chain.reduce((feature, { f, args }) => f(feature, args), geometry);
@@ -52,7 +52,7 @@ async function cleanUp(input, options = {}) {
  *
  * @param {String} input
  * @param {{bbox?: number[], step?: number, truncate?: boolean | number, skipCleanup?: boolean} | undefined} options
- * @returns {turf.FeatureCollection | Promise<turf.FeatureCollection>}
+ * @returns {GeoJSON.FeatureCollection | Promise<GeoJSON.FeatureCollection>}
  */
 
 module.exports = function getSvgWrapper(input, options = {}) {
